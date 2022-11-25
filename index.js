d3b116c165f030f5cda032c25c0eaf8a43534f76
RGBrain <31857067+RGBrain@users.noreply.github.com>
2022-11-25 10:21:09 +0000

    all new project files added

---
+++ index.js
@@ -0,0 +1,94 @@
+// rock paper scissors
+
+// Initialise game counters
+var userWins = 0;
+var userLosses = 0;
+var ties = 0;
+
+// Run game ten times
+for (var i = 0; i < 10; i++) {
+    // Randomly generate computer choice
+    var computerOptions = ["rock", "paper", "scissors"];
+    var randomNumber = Math.random();
+    var computerChoice = computerOptions[Math.floor(randomNumber * 3)];
+
+    // Get user choice
+    var validChoice = false;
+    var userChoice = prompt("rock, paper or scissors?");
+    userChoice = userChoice.toLowerCase();
+    
+    // Announce choices...
+    if  (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        validChoice = true;  
+        alert("You chose " + userChoice + ".  The computer chose " + computerChoice + ".");
+    }
+    // ...or alert for invalid input 
+    else {
+        alert("You didn't enter a valid input.  The computer chose " + computerChoice + ", but it's pretty academic at this point.  You still have " + (10-i) + " chances.  Have another try...");
+    }
+
+
+    // Check for a draw
+    if (userChoice === computerChoice) {
+        ties++;
+        alert("This is a draw");
+    } 
+    
+    // Ascertain winner
+    else if (userChoice === "rock") {
+            if (computerChoice === "scissors") {
+            userWins++;
+            alert(
+                "You win - you now have " + userWins + " win/s!"
+                );
+            } else {
+                userLosses++;
+                alert(
+                "Unlucky, your rock has been papered!  You have now lost " +
+                userLosses +
+                " time/s."
+            );
+            }
+    }
+    else if (userChoice === "paper") {
+            if (computerChoice === "scissors") {
+                userLosses++;
+                alert(
+                    "Oh dear, you lost - that's " + userLosses + " loses in total now"
+                );
+            } else {
+                userWins++;
+                alert(
+                    "BOOOOOM, that's " + userWins + " now!"
+                );
+            }
+    }
+    else if (userChoice === "scissors") {
+            if (computerChoice === "paper") {
+                userWins++;
+                alert(
+                    "Like scissors through paper! You now have " + userWins + " wins - Tremenous!"
+                    );
+            } else {
+                userLosses++;
+                alert("Those rocks are tough!  The computer has beaten you " + userLosses + " times.");
+            }
+    }
+
+        var winRate = (userWins / (i+1)) * 100;
+        console.log("Games played: " + (i+1) + ".  wins: " + userWins + ".  You have won " + winRate.toFixed(2) + "% of your games.");
+
+
+    //   console.log("The value of userChoice is " + userChoice);
+    //   console.log("The value of computerChoice is " + computerChoice);
+    //   console.log("Played: " + i);
+    //   console.log("Won: : " + userWins);
+    //   console.log("Lost: " + userLosses);
+
+}
+
+alert("Games played: " + (i) + ".  wins: " + userWins + ".  loses: " + userLosses + ". Ties: " + ties + ".  You won " + winRate.toFixed(2) + "% of your games.");
+if (((i) / (userWins + userLosses + ties)) % 10 != 1) {
+    alert(i)
+    alert("...oh, and work on that spelling ;)")
+}
